8bdaa7a9393e8bc65f5873085f4d99a490b186fb
saps999 <saptarshibiswasgang@gmail.com>
2022-09-24 05:27:49 +0530

    First name starts with Cap and has minimum 3 characters

--- src/com/bridgelabz/RegexProblems.java
+++ src/com/bridgelabz/RegexProblems.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class RegexProblems {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter First name: ");
+        boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
+        System.out.println("Result : " + firstName);
     }
 }
