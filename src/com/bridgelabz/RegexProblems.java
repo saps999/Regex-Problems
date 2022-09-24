345bdd04f228e4b96e7d09ed78e5b1864e721eb7
saps999 <saptarshibiswasgang@gmail.com>
2022-09-24 05:41:10 +0530

    E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl

--- src/com/bridgelabz/RegexProblems.java
+++ src/com/bridgelabz/RegexProblems.java
@@ -16,5 +16,11 @@ public class RegexProblems {
         System.out.print("Enter last name: ");
         boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + lastName);
+
+        System.out.println(" ");
+
+        System.out.print("Enter your e-mail id: ");
+        boolean email = Pattern.matches("[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
+        System.out.println("Result : " + email);
     }
 }
