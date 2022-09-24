8659acb5cdd6cacfada4beea4a054a01b0b1aef3
saps999 <saptarshibiswasgang@gmail.com>
2022-09-24 05:34:32 +0530

    Last name starts with Cap and has minimum 3 characters

--- src/com/bridgelabz/RegexProblems.java
+++ src/com/bridgelabz/RegexProblems.java
@@ -7,8 +7,14 @@ public class RegexProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter First name: ");
+        System.out.print("Enter First name: ");
         boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + firstName);
+
+        System.out.println(" ");
+
+        System.out.print("Enter last name: ");
+        boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
+        System.out.println("Result : " + lastName);
     }
 }
