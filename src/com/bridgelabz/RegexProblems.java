53989c7672af0487b595571bc362a8bbf220abf9
saps999 <saptarshibiswasgang@gmail.com>
2022-09-24 06:31:41 +0530

    Country-code follow by space and 10 digit number

--- src/com/bridgelabz/RegexProblems.java
+++ src/com/bridgelabz/RegexProblems.java
@@ -6,27 +6,39 @@ import java.util.regex.Pattern;
 public class RegexProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        Scanner scanner =new Scanner(System.in);
+        /*
+        First name starts with Cap and has minimum 3 characters
+         */
         System.out.print("Enter First name: ");
         boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + firstName);
 
         System.out.println(" ");
-
+        /*
+        Last name starts with Cap and has minimum 3 characters
+         */
         System.out.print("Enter last name: ");
         boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + lastName);
 
         System.out.println(" ");
-
+        /*
+         Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+         */
         System.out.print("Enter your e-mail id: ");
-        boolean email = Pattern.matches("[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
+        boolean email = Pattern.matches("[a-zA-Z0-9.]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
         System.out.println("Result : " + email);
 
         System.out.println(" ");
+        /*
+         Country code follow by space and 10 digit number
+         */
 
         System.out.print("Enter your Mobile number: ");
-        boolean mobileNo = Pattern.matches("[0-9]{1,15}\s[9876]{1}[0-9]{9}", sc.nextLine());
+        boolean mobileNo = Pattern.matches("[0-9]{1,15}\s[9876]{1}[0-9]{9}",scanner.nextLine());
         System.out.println("Result : " + mobileNo);
+
+        System.out.println(" ");
     }
 }
